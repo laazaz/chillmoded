5afed629d8f7faa57243309371f2740efd40dc4d
Ayoub Laazazi <31414432+laazaz@users.noreply.github.com>
2024-08-10 13:42:41 +0100

    Update modded2.js

--- modded2.js
+++ modded2.js
@@ -21184,7 +21184,6 @@
           , x = n(16497);
         let N = 0;
 		window.lemonGOC = u.rV.gameObjectClicked;
-		window.lemon20823 = n(20823);
 		window.lemonN = n;
         u.rV.gameObjectClicked.subscribe(e=>{
 		if(window.logGOC) console.log(e)
